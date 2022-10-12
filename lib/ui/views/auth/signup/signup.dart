b590b79ff227111eec5c6e1681c1cdd608e2a8be
Oluwatunmise Dunmade <oluwatunmisedunmade2017@gmail.com>
2022-10-12 01:03:33 +0100

    Signup

--- lib/ui/views/auth/signup/signup.dart
+++ lib/ui/views/auth/signup/signup.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+
+import 'otp_page.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -10,6 +14,253 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Center(
+                child: Image.asset('assets/images/signuptop.png'),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: Image.asset('assets/images/signupleft.png'),
+                ),
+                Container(
+                  child: Image.asset(
+                    'assets/images/Kolologo.png',
+                    height: 106,
+                    width: 82.3,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 47.0),
+                  child: Container(
+                    child: Image.asset('assets/images/signupright.png'),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+                padding: EdgeInsets.only(
+                  top: 20,
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 258.0,
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              'Name',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 300,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Color(0xFFc4c4c4)),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.person_outline,
+                                  color: Color(0xFFc4c4c4),
+                                ),
+                                border: InputBorder.none,
+                                labelText: "Full Name",
+                                labelStyle: TextStyle(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 258.0,
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              'Email',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 300,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Color(0xFFc4c4c4)),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.mail_outline,
+                                  color: Color(0xFFc4c4c4),
+                                ),
+                                border: InputBorder.none,
+                                labelText: "Email",
+                                labelStyle: TextStyle(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 231.0,
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              'Password',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 300,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Color(0xFFc4c4c4)),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.lock_outline,
+                                  color: Color(0xFFc4c4c4),
+                                ),
+                                suffixIcon: Icon(
+                                  Icons.remove_red_eye_outlined,
+                                ),
+                                border: InputBorder.none,
+                                labelText: "Password",
+                                labelStyle: TextStyle(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 180.0,
+                              bottom: 10.0,
+                            ),
+                            child: Text(
+                              'Confirm Password',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15.0,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: 300,
+                            height: 45,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Color(0xFFc4c4c4)),
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                prefixIcon: Icon(
+                                  Icons.lock_outline,
+                                  color: Color(0xFFc4c4c4),
+                                ),
+                                suffixIcon: Icon(
+                                  Icons.remove_red_eye_outlined,
+                                ),
+                                border: InputBorder.none,
+                                labelText: "Confirm Password",
+                                labelStyle: TextStyle(
+                                  color: Color(0xFFC4C4C4),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.only(
+                              top: 30,
+                            ),
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                    context,
+                                    // MaterialPageRoute(builder: (context) => HomePage()));
+                                    MaterialPageRoute(
+                                        builder: (context) => OtpScreen()));
+                              },
+                              child: SizedBox(
+                                height: 50.0,
+                                width: 300.0,
+                                child: Material(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  color: Color(0xFF3b2c77),
+                                  elevation: 7.0,
+                                  child: Center(
+                                    child: Text(
+                                      'Sign up',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/ui/views/onboarding/onboarding_page.dart
+++ lib/ui/views/onboarding/onboarding_page.dart
@@ -112,9 +112,136 @@ class _OnboardingPageState extends State<OnboardingPage> {
               ),
             ),
           ),
-          _controller.isMiddlePage
-              ? 
-               Padding(
+          _controller.isFirstPage
+              ? Padding(
+                  padding: const EdgeInsets.only(top: 340, left: 27),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              // MaterialPageRoute(builder: (context) => HomePage()));
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()));
+                        },
+                        child: Container(
+                          height: 50.0,
+                          width: 340.0,
+                          child: Material(
+                            borderRadius: BorderRadius.circular(10.0),
+                            color: Color(0xFF3b2c77),
+                            elevation: 7.0,
+                            child: Center(
+                              child: Text(
+                                'Log in',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              // MaterialPageRoute(builder: (context) => HomePage()));
+                              MaterialPageRoute(
+                                  builder: (context) => SignUp()));
+                        },
+                        child: Container(
+                          height: 50.0,
+                          width: 340.0,
+                          color: Colors.white,
+                          child: Container(
+                            decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color.fromARGB(255, 213, 213, 213),
+                                    blurRadius: 2,
+                                  ),
+                                ],
+                                border: Border.all(
+                                  color: Colors.white,
+                                  style: BorderStyle.solid,
+                                  width: 0,
+                                ),
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10.0)),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Center(
+                                  child:
+                                      Image.asset('assets/images/Google.png'),
+                                ),
+                                SizedBox(
+                                  width: 5.0,
+                                ),
+                                Center(
+                                  child: Text(
+                                    'Sign up',
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                        fontFamily: 'Montserrat'),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                      // Padding(
+                      //   padding: const EdgeInsets.only(top: 20.0),
+                      //   child: Container(
+                      //     decoration: BoxDecoration(
+                      //         boxShadow: [
+                      //           BoxShadow(
+                      //             color: Color.fromARGB(255, 213, 213, 213),
+                      //             blurRadius: 2,
+                      //           ),
+                      //         ],
+                      //         border: Border.all(
+                      //           color: Colors.white,
+                      //           style: BorderStyle.solid,
+                      //           width: 0,
+                      //         ),
+                      //         color: Colors.white,
+                      //         borderRadius: BorderRadius.circular(10.0)),
+                      //     child: TextButton(
+                      //       onPressed: () {
+                      //         Navigator.push(
+                      //           context,
+                      //           MaterialPageRoute(
+                      //               builder: (context) => LoginPage()),
+                      //         );
+                      //       },
+                      //       child: Obx(
+                      //         () {
+                      //           return Text(
+                      //               _controller.isLastPage ? 'Start' : 'Next',
+                      //               style: TextStyle(
+                      //                 color: Color(0xFF3b2c77),
+                      //                 fontWeight: FontWeight.bold,
+                      //                 fontSize: 18,
+                      //               ));
+                      //         },
+                      //       ),
+                      //     ),
+                      //   ),
+                      // )
+                    ],
+                  ),
+                )
+              : Padding(
                   padding: const EdgeInsets.only(top: 520.0),
                   child: Container(
                     width: double.infinity,
